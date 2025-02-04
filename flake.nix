0685c1c08f35bea2fd5502e85a772af5393b22b1
BJSummerfield <brianjsummerfield@gmail.com>
2025-02-03 20:14:38 -0500

    test

---
+++ flake.nix
@@ -0,0 +1,93 @@
+{
+  description = "Example nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager, nix-homebrew }:
+  let
+    configuration = { pkgs, ... }: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = with pkgs; [
+        helix
+        zoxide
+        fish
+        git
+        lazygit
+        starship
+        ghostty
+      ];
+
+      homebrew = {
+        enable = true;
+        casks = [
+          "1password"
+          "keybase"
+          "microsoft-teams"
+          "firefox@developer-edition"
+          "aerospace"
+        ];
+        onActivation.cleanup = "zap";
+
+        masApps = {
+          "Okta Verify" = 490179405
+        }
+      };
+      fonts.packages = with pkgs; [
+        nerd-fonts.monaspace
+      ];
+       
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      users.users.davish = {
+          name = "brian";
+          home = "/Users/brian";
+      };      
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."simple" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+
+        nix-homebrew.darwinModules.nix-homebrew = {
+          enable = true;
+          enableRosetta = true;
+          user = "brian";
+        };
+
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.brian = import ./home.nix;
+        };
+      ];  
+    };
+  };
+}
