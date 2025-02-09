59c33693d04a28711674dc6e263146d23d68d5c9
BJSummerfield <brianjsummerfield@gmail.com>
2025-02-09 11:15:45 -0600

    update lg colors

--- home.nix
+++ home.nix
@@ -47,14 +47,14 @@
         language = "en";
         theme = {
           activeBorderColor = [
-            "#f5e0dc"
+            "#a6e3a1"
             "bold"
           ];
           inactiveBorderColor = [ "#a6adc8" ];
           optionsTextColor = [ "#89b4fa" ];
           selectedLineBgColor = [ "#313244" ];
           cherryPickedCommitBgColor = [ "#45475a" ];
-          cherryPickedCommitFgColor = [ "#f5e0dc" ];
+          cherryPickedCommitFgColor = [ "#a6e3a1" ];
           unstagedChangesColor = [ "#f38ba8" ];
           defaultFgColor = [ "#cdd6f4" ];
           searchingActiveBorderColor = [ "#f9e2af" ];
